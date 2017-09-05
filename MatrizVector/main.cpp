8e66932afdb2484c99403f9366a275c375067cec
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2017-09-04 22:23:56 -0700

    ...

--- MatrizVector/main.cpp
+++ MatrizVector/main.cpp
@@ -15,7 +15,7 @@ int main()
     // Punteros a matrices, inicializados en NULL para proteccion
     float *A=NULL,*B=NULL,*C=NULL;
     //Dimensiones
-    int renA,colA,dimA,renC,colC;
+    int renA,colA,dim,renC,colC;
 
     //Nombres de matrices
     char matrizA[] = {"matrizA.txt"};
@@ -24,7 +24,7 @@ int main()
     //leyendo matrices
 
     A = leer(matrizA,&renA,&colA);
-    B = leer(vectorA,&dimA);
+    B = leer(vectorA,&dim);
 /*
     if(!A|| !B){
         std::cout << "Falla en lectura" << std::endl;
@@ -34,8 +34,7 @@ int main()
     pintar(A,renA,colA);
     std::cout << std::endl;
     std::cout << std::endl;
-    //pintar(B,renB,colB);
-
+    pintar(B,dim);
     // Se crea una matriz de dimensiones compatibles a la operacion a realizar
 
     //renC = renA;
@@ -48,21 +47,30 @@ int main()
     //prod_mat_vec(A,B,C,renA,colA);
     //prod_mat_mat_1(A,B,C,renA,colA,colB);
     //C = prod_mat_mat_archivo(matrizA, &renC, &colC);
-    hacer_uno(A,colA);
+    //hacer_uno(A,colA);
+    //hacer_cero(A, A+colA, colA);
 
+    ///REDUCIR
+    reducir(A,B, dim);
+/*
+    float *p1,*q1, valor;
+    p1 = A;
+    q1 = B;
 
+    valor = hacer_uno(p1, dim);
+    //Modificamos el vector con el valor el cual manipulamos la matriz
+    *q1 = (*q1) * valor;
+
+    valor = hacer_cero(p1, p1 + dim, dim);
+    //Modificamos el elemento del vector que corresponde
+    *(q1 + 1) = *(q1 + 1) + valor * (*q1);
+*/
     std::cout << std::endl;
     std::cout << std::endl;
     pintar(A,renA,colA);
-
-    hacer_cero(A, A+colA, colA);
-
-
     std::cout << std::endl;
     std::cout << std::endl;
-    pintar(A,renA,colA);
-    //escribir("matrizC.txt",C,renC,colC);
-
+    pintar(B,dim);
 
     ///Liberacion de memoria
     free(A);
