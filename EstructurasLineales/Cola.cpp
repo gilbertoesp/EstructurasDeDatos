6a78eaeb52e1663b67628e9f005d5827262193ae
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2017-09-11 15:05:05 -0700

    Agregado archivos de la estructura Cola

---
+++ EstructurasLineales/Cola.cpp
@@ -0,0 +1,10 @@
+/**
+    Cola.cpp
+    Estructura Lineal de tipo cola, FIFO
+    Gilberto Espinoza
+*/
+#include <cstdlib>
+#include <iostream>
+//Estructuras
+#include "Caja.h"
+#include "Cola.h"
---
+++ EstructurasLineales/Cola.h
@@ -0,0 +1,15 @@
+#ifndef COLA_H_INCLUDED
+#define COLA_H_INCLUDED
+
+//Incluyendo estructura base
+#include "Caja.h"
+
+class Cola{
+    Caja *principio, *final;
+    int cuantos;
+
+public:
+
+};
+
+#endif // COLA_H_INCLUDED
