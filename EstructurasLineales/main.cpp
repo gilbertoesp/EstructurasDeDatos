9210f6b82702d8ddc2e64588ceb1ef7113de7dfa
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2017-09-13 15:00:43 -0700

    Agregado codigo para pruebas de Cola y Lista Ordenanda

--- EstructurasLineales/main.cpp
+++ EstructurasLineales/main.cpp
@@ -6,6 +6,9 @@
 #include <iostream>
 //Librerias de la estructuras
 #include "Pila.h"
+#include "Cola.h"
+#include "ListaOrdenada.h"
+
 int main()
 {
     std::cout << "\t\tESTRUCTURAS LINEALES\n" << std::endl;
@@ -18,18 +21,54 @@ int main()
     pila.agregar(5);
     pila.agregar(9);
     pila.agregar(4);
-    std::cout << "Pintando Pila: ";
+    std::cout << "\nPintando Pila: ";
     pila.pintar();
     std:: cout << std::endl;
     std::cout << "Cantidad de elementos en la Pila: " << pila.cuantosSon() << std::endl;
     std::cout << std::endl << "Sacando el valor: " << pila.sacar() << std::endl;
-    std::cout << "Pintando Pila: ";
+    std::cout << "\nPintando Pila: ";
     pila.pintar();
     std:: cout << std::endl;
     std::cout << "Cantidad de elementos en la Pila: " << pila.cuantosSon() << std::endl;
 
     std::cout << "\n\n\t\t\tCOLA\n" << std::endl;
+    std::cout << "Agregando 8, 5, 9, 4 a la Cola" << std::endl;
+    Cola cola;
+    cola.agregar(8);
+    cola.agregar(5);
+    cola.agregar(9);
+    cola.agregar(4);
+    std::cout << "\nPintando Cola: " << std::endl;
+    cola.pintar();
+    std::cout << std::endl;
+    std::cout << "Cantidad de elementos en la Cola: " << cola.cuantosSon() << std::endl;
+    std::cout << "\nSacando el primer elemento de la Cola: " << cola.sacar() << std::endl;
+    std::cout << "\nPintando Cola: " << std::endl;
+    cola.pintar();
+    std::cout << std::endl;
+    std::cout << "Cantidad de elementos en la Cola: " << cola.cuantosSon() << std::endl;
 
+    std::cout << "\n\n\t\t\tLISTA ORDENADA\n" << std::endl;
+    std::cout << "Agregando 8, 5, 9, 4, 7 a la Lista" << std::endl;
+    ListaOrdenada lista;
+    lista.agregar(8);
+    lista.agregar(5);
+    lista.agregar(9);
+    lista.agregar(4);
+    lista.agregar(7);
+    std::cout << "\nPintando Lista: " << std::endl;
+    lista.pintar();
+    std::cout << std::endl;
+    std::cout << "Cantidad de elementos en la Lista: " << lista.cuantosSon() << std::endl;
+    std<<cout << "\nSacando el primer valor de la lista: " << lista.sacar() << std::endl;
+    std::cout << "\nPintando Lista: " << std::endl;
+    lista.pintar();
+    std::cout << std::endl;
+    std::cout << "Eliminando el elemento 8" << std::endl;
+    lista.borrar(8);
+    std::cout << "\nPintando Lista: " << std::endl;
+    lista.pintar();
+    std::cout << std::endl;
 
 
     system("pause");
