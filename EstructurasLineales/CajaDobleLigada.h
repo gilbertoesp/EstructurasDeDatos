7357fcad54b2e96e28ff360583db8a2e71f9165c
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2017-09-25 23:17:15 -0700

    Caja especial con punteros para desplazamiento hacia delante y hacia atras

---
+++ EstructurasLineales/CajaDobleLigada.h
@@ -0,0 +1,13 @@
+#ifndef CAJADOBLELIGADA_H_INCLUDED
+#define CAJADOBLELIGADA_H_INCLUDED
+/**
+    Estructura base para las Estructuras lineales de datos, Pila, Cola y Lista Ordenada
+    la variable (int) valor, puede ser reemplazada segun se necesite para la aplicacion de pila o cola
+    Esta version de Caja, contiene un puntero extra para un desplazamiento hacia atras al igual que hacia delante
+*/
+struct CajaDobleLigada{
+    int valor;
+    CajaDobleLigada *siguiente;
+    CajaDobleLigada *anterior;
+};
+#endif // CAJADOBLELIGADA_H_INCLUDED
