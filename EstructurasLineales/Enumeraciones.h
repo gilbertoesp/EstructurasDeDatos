be5b69e83cfe0fceec65c016dbf39ca8b9e8a9c6
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2017-09-25 18:52:52 -0700

    Cabecera que incluye las definiciones de Posicion y Boolean

---
+++ EstructurasLineales/Enumeraciones.h
@@ -0,0 +1,10 @@
+#ifndef ENUMERACIONES_INCLUDED
+#define ENUMERACIONES_INCLUDED
+
+/// Enumeraciones necesarias para ubicarnos en las Listas
+//*******************************************************************************************
+enum Posicion{VACIO,PRINCIPIO,EN_MEDIO,FINAL};
+enum Boolean{NO,SI};
+//*******************************************************************************************
+
+#endif // ENUMERACIONES_INCLUDED
