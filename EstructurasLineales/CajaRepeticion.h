9e0029790dd1fe14b68819a76df028d12ad1c4fe
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2017-09-25 18:52:18 -0700

    Caja especial para estructuras con alta repeticion de Datos

---
+++ EstructurasLineales/CajaRepeticion.h
@@ -0,0 +1,13 @@
+#ifndef CAJAREPETICION_H_INCLUDED
+#define CAJAREPETICION_H_INCLUDED
+/**
+    Estructura base para las Estructuras lineales de datos, Pila, Cola y Lista Ordenada
+    la variable (int) valor, puede ser reemplazada segun se necesite para la aplicacion de pila o cola.
+    Esta variente de Caja, contiene a (int) cuantos, que representa la cantidad de veces que se repite un Dato
+*/
+struct CajaRepeticion{
+    int valor;
+    int cuantos;
+    CajaRepeticion *siguiente;
+};
+#endif // CAJAREPETICION_H_INCLUDED
