56f7c531c0bdff754477caa83fa2afc5a1b64a47
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2017-10-04 13:32:13 -0700

    Agregada 'Caja1', que sirve de base para 'ListaArcos'

--- Grafica/CajaArco.h
+++ Grafica/CajaArco.h
@@ -2,23 +2,23 @@
 #define CAJAARCO_H_INCLUDED
 
 /**
-	...
+	Estructura que hace referencia a un nodo, ubicado con (int) id.
+	Esta estructura es auxiliar a los objetos tipo "Grafica". Se puede organizar como si de una Lista Ordenada
+	se tratase gracias a la variable (CajaArco *) siguiente.
+
 
 	\author Gilberto Espinoza
 */
 //*************************************************************************************************
 /**
 	...
-	\param
-	\return
+	\param id           : Identificador del nodo al que esta estructura hacce referencia
+	\param siguiente    : Si se necesita, siguiente en una supuesta lista de arcos. Default = NULL
 */
-
-//*************************************************************************************************
-/**
-	...
-	\param
-	\return
-*/
+struct CajaArco{
+    int id;
+    CajaArco * siguiente = NULL;
+};
 
 //*************************************************************************************************
 
